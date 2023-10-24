7961dc4b0c863eae7be6aeead90fbf0252dac4dd
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2023-10-24 13:15:48 +0600

    add: компонент стандартного хедера страниц

---
+++ frontend/src/components/HeadPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="head-page">
+    <h1 class="head-page__title">{{ props.title }}</h1>
+    <div class="flex gap-2">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+interface IProps {
+  title: string,
+};
+
+const props = defineProps<IProps>();
+</script>
+
+<style scoped>
+.head-page {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px 20px;
+}
+
+.head-page__title {
+  font-size: 22px;
+  font-weight: 600;
+}
+</style>
--- frontend/src/views/users/components/HeadUsersPage.vue
+++ frontend/src/views/users/components/HeadUsersPage.vue
@@ -1,19 +1,17 @@
 <template>
-  <div class="users__head">
-    <h1 class="users__title">Админы</h1>
-    <div class="flex gap-2">
-      <button class="button" @click="$emit('addUser')">
-        <svg class="button__icon">
-          <use xlink:href="@/assets/icons/sprites/buttons.svg#add"></use>
-        </svg>
-        Добавить
-      </button>
-    </div>
-  </div>
+  <HeadPage title="Админы">
+    <button class="button" @click="$emit('addUser')">
+      <svg class="button__icon">
+        <use xlink:href="@/assets/icons/sprites/buttons.svg#add"></use>
+      </svg>
+      Добавить
+    </button>
+  </HeadPage>
 </template>
 
 <script lang="ts" setup>
 import { defineEmits } from 'vue';
+import HeadPage from '@/components/HeadPage.vue';
 
 interface IEmits {
   (e: 'addUser'): void
@@ -21,17 +19,3 @@ interface IEmits {
 
 const emit = defineEmits<IEmits>();
 </script>
-
-<style scoped>
-.users__head {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 16px 20px;
-}
-
-.users__title {
-  font-size: 22px;
-  font-weight: 600;
-}
-</style>
