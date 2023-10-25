d7346f7c4f449a94c7360e12868e3cf571802a95
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2023-10-25 11:04:05 +0600

    add: вспомогательные типы для юзера

--- frontend/src/entities/types/backend/user.ts
+++ frontend/src/entities/types/backend/user.ts
@@ -1,3 +1,13 @@
+export enum UserState {
+  active = "ACTIVE",
+  deleted = "DELETED",
+};
+
+export enum UserRoles {
+  superAdmin = 'SUPERADMIN',
+  admin = 'ADMIN',
+};
+
 export interface IUser {
   id: number,
   createdDate: string,
@@ -6,4 +16,14 @@ export interface IUser {
   lastName: string,
   login: string,
   password: string,
+  state: UserState,
+  role: UserRoles,
+  avatar: string,
+};
+
+export interface IProfile {
+  id: number,
+  firstName: string,
+  lastName: string,
+  avatar: string,
 };
