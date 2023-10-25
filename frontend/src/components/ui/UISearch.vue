4484a8a506ed20ca4bdfaa03618e4b211ed08964
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2023-10-25 16:06:40 +0600

    add: UISearch компонент

---
+++ frontend/src/components/ui/UISearch.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="relative w-60">
+    <input
+      type="text"
+      class="input input--with-icon"
+      placeholder="Поиск..."
+    />
+    <svg class="input__icon">
+      <use xlink:href="@/assets/icons/sprites/inputs.svg#search"></use>
+    </svg>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+@import url('@/assets/styles/blocks/input.css');
+</style>
