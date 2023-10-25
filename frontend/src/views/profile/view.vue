e4cab6c98e94454f83a9eb0e7c2451eff35b186b
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2023-10-25 11:04:13 +0600

    add: страница профиля

--- frontend/src/views/profile/view.vue
+++ frontend/src/views/profile/view.vue
@@ -1,10 +1,124 @@
 <template>
-  Профиль
+  <div class="profile">
+    <HeadPage title="Профиль" />
+
+    <div class="profile__content">
+      <div class="profile__img">
+        <svg>
+          <use xlink:href="@/assets/icons/sprites/profile.svg#profile"></use>
+        </svg>
+      </div>
+
+      <div class="profile__data">
+        <div v-if="userData" class="flex flex-col gap-2">
+          <UIInput
+            v-model="userData.firstName"
+            label="Имя:"
+            required
+            autocomplete="name"
+          />
+
+          <UIInput
+            v-model="userData.lastName"
+            label="Фамилия:"
+            required
+            autocomplete="family-name"
+          />
+        </div>
+      </div>
+
+      <div class="profile__controls">
+        <button
+          class="button button--gray"
+          @click="goBackupData"
+        >
+          Сбросить
+        </button>
+        <button
+          class="button"
+          @click="saveNewUserData"
+        >
+          Сохранить
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup>
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue';
+
+import type { IUser } from '@/entities/types/backend/user';
+
+import HeadPage from '@/components/HeadPage.vue';
+import UIInput from '@/components/ui/UIInput.vue';
+
+import { getUserById, saveProfile } from '@/api/users';
+
+const userData = ref<IUser | null>(null);
+let backupUserData: IUser | null = null;
+
+async function fetchUserData() {
+  userData.value = await getUserById(1);
+  backupUserData = userData.value;
+}
+
+function goBackupData() {
+  userData.value = backupUserData;
+}
+
+function saveNewUserData() {
+  if (userData.value) {
+    saveProfile({
+      id: userData.value.id,
+      firstName: userData.value.firstName,
+      lastName: userData.value.lastName,
+      avatar: userData.value.avatar,
+    });
+  }
+}
+
+onMounted(() => {
+  fetchUserData();
+});
 </script>
 
 <style scoped>
+.profile__content {
+  position: relative;
+  background-color: var(--bg-main);
+  padding: 12px;
+  margin: 12px;
+  display: flex;
+  gap: 20px;
+  border-radius: 12px;
+  align-items: flex-start;
+}
 
+.profile__img {
+  width: 360px;
+  height: 460px;
+  border-radius: 16px;
+  border: 2px solid var(--border-standard);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.profile__img > svg {
+  width: 140px;
+  height: 140px;
+}
+
+.profile__data {
+  position: relative;
+}
+
+.profile__controls {
+  display: flex;
+  gap: 8px;
+  position: absolute;
+  right: 16px;
+  bottom: 16px;
+}
 </style>
