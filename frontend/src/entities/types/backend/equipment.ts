5f58381d9d9e80367ae566438e1309454b6b80a1
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2023-10-28 08:52:19 +0600

    add: типы для оборудования

---
+++ frontend/src/entities/types/backend/equipment.ts
@@ -0,0 +1,28 @@
+import type { IBranch } from './branches';
+import type { IUser } from './user';
+
+export interface IHistoryItem {
+  id: number,
+  date: string, // когда была передана
+  passedOn: IUser, // передал
+  accepted: string, // принял
+};
+
+export interface IEquipment {
+  id: number,
+  createdDate: string, // дата ввода в эксплуатацию
+  updatedDate: string, // дата последнего изменения
+
+  assetNumber: string, // ассет номер
+  inventoryNumber: string, // инвентарный номер
+  name: string, // название
+  description: string, // описание
+  serialNumber: string, // серийный номер
+  financiallyResponsiblePerson: string, // материально ответственное лицо
+  recipient: string, // получатель
+  comments: string[] // комментарии
+  invoiceNumber: string, // номер накладной
+  branch: IBranch, // филиал
+
+  history: IHistoryItem[],
+};
