4c4d3afbf1529be405a604d92d7bcc10581b498e
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2023-11-04 17:28:03 +0600

    add: компонент селекта

---
+++ frontend/src/components/ui/UISelect.vue
@@ -0,0 +1,40 @@
+<template>
+  <label class="input__label">
+    <span class="text-sm">{{ props.label }}</span>
+    <select
+      class="input"
+      v-bind="$attrs"
+      :value="modelValue"
+      @change="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+    >
+      <option
+        v-for="option in options"
+        :key="option.key"
+      >
+        {{ option.title }}
+      </option>
+    </select>
+  </label>
+</template>
+
+<script lang="ts" setup>
+import { defineProps, defineEmits } from 'vue';
+import type { IOptions } from '@/entities/types/UI/select';
+
+interface IProps {
+  label: string,
+  modelValue: string,
+  options: IOptions[]
+};
+
+interface IEmits {
+  (e: 'update:modelValue', newValue: string): void,
+};
+
+const props = defineProps<IProps>();
+const emits = defineEmits<IEmits>();
+</script>
+
+<style scoped>
+@import url('@/assets/styles/blocks/input.css');
+</style>
