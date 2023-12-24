d477ee7debe748251951cd4155d1f11999bdf512
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2023-12-24 12:55:35 +0600

    add: форматирование даты с помощью Intl.

---
+++ frontend/src/utils/helpers/date.ts
@@ -0,0 +1,38 @@
+export enum TimeFormatDict {
+  StandardTime = 'standardTime',
+  FullTime = 'fullTime',
+  FullDateLongMonth = 'fullDateLongMonth',
+  FullDateNumeric = 'fullDateNumeric',
+};
+
+export const TimeFormats = {
+  [TimeFormatDict.StandardTime]: {
+    minute: 'numeric',
+    hour: 'numeric',
+  },
+  [TimeFormatDict.FullTime]: {
+    minute: 'numeric',
+    hour: 'numeric',
+    second: 'numeric',
+  },
+  [TimeFormatDict.FullDateLongMonth]: {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  },
+  [TimeFormatDict.FullDateNumeric]: {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  },
+} as const;
+
+function getFormatDate(date: string, format: TimeFormatDict) {
+  const result = new Date(date);
+
+  return new Intl.DateTimeFormat("ru-RU", TimeFormats[format]).format(result);
+}
+
+export {
+  getFormatDate,
+};
