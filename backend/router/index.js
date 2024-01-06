4aebf1a628047f32cb69b37b9240e46304cdd739
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-01-06 09:18:23 +0600

    add: router urls

--- backend/router/index.js
+++ backend/router/index.js
@@ -6,4 +6,28 @@ router.get('/ping', (request, response) => {
   response.status(200).json('pong');
 });
 
+// филиалы
+router.get('/branches', () => {}); // @todo: пагинация
+router.get('/branches/search', () => {}); // @todo: пагинация
+router.get('/branch/:id', () => {});
+router.post('/branch', () => {});
+router.patch('/branch/:id', () => {});
+router.delete('/branch/:id', () => {});
+
+// юзеры
+router.get('/users', () => {}); // @todo: пагинация
+router.get('/users/search', () => {}); // @todo: пагинация
+router.get('/user/:id', () => {});
+router.post('/user', () => {});
+router.patch('/user/:id', () => {});
+router.delete('/user/:id', () => {});
+
+// оборудование
+router.get('/equipments', () => {}); // @todo: пагинация
+router.get('/equipments/search', () => {}); // @todo: пагинация
+router.get('/equipment/:id', () => {});
+router.post('/equipments', () => {}); // @todo: тут файл должен быть
+router.patch('/equipment/:id', () => {});
+router.delete('/equipment/:id', () => {});
+
 export default router;
