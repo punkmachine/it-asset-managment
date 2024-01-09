8003bb78af94270e21cfead8aa1339d4b92a44a7
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-01-09 17:54:01 +0600

    add: auth middleware in router

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { check } from 'express-validator';
 
 import authMiddleware from './middleware/auth.js';
+// import catchMiddleware from './middleware/catch.js'
 
 import AuthController from './controllers/auth.js';
 import UserController from './controllers/user.js';
@@ -9,32 +10,30 @@ import BranchesController from './controllers/branch.js';
 
 const router = new Router();
 
-router.get('/ping', (request, response) => {
-  response.status(200).json('pong');
-});
-
 router.post('/login', AuthController.login);
 
 // филиалы
-router.get('/branches', BranchesController.getAll); // @todo: пагинация
-router.get('/branches/search', () => {}); // @todo: пагинация
-router.get('/branch/:id', BranchesController.getById);
-router.post('/branch', BranchesController.create);
-router.put('/branch/:id', BranchesController.updateById);
-router.delete('/branch/:id', BranchesController.deleteById);
+router.get('/branches', authMiddleware, BranchesController.getAll); // @todo: пагинация
+router.get('/branches/search', authMiddleware, () => {}); // @todo: пагинация
+router.get('/branch/:id', authMiddleware, BranchesController.getById);
+router.post('/branch', authMiddleware, BranchesController.create);
+router.put('/branch/:id', authMiddleware, BranchesController.updateById);
+router.delete('/branch/:id', authMiddleware, BranchesController.deleteById);
 
 // юзеры
-router.get('/users', UserController.getAll); // @todo: пагинация
-router.get('/users/search', () => {}); // @todo: пагинация
-router.get('/user/:id', UserController.getById);
-router.post('/user', [
+router.get('/users', authMiddleware, UserController.getAll); // @todo: пагинация
+router.get('/users/search', authMiddleware, () => {}); // @todo: пагинация
+router.get('/user/:id', authMiddleware, UserController.getById);
+router.post('/user',
+  [
     check('login', "Имя пользователя не может быть пустым").notEmpty(),
-    check('password', "Пароль должен быть больше 4 и меньше 10 символов").isLength({ min:4 }),
+    check('password', "Пароль должен быть больше 4 и меньше 10 символов").isLength({ min: 4 }),
+    authMiddleware
   ],
   UserController.create
 );
-router.put('/user/:id', UserController.updateById);
-router.delete('/user/:id', UserController.deleteById);
+router.put('/user/:id', authMiddleware, UserController.updateById);
+router.delete('/user/:id', authMiddleware, UserController.deleteById);
 
 // оборудование
 router.get('/equipments', () => {}); // @todo: пагинация
@@ -43,6 +42,9 @@ router.get('/equipment/:id', () => {});
 router.post('/equipments', () => {}); // @todo: тут файл должен быть
 router.put('/equipment/:id', () => {});
 router.delete('/equipment/:id', () => {});
+
+// комменты оборудования
+router.post('/equipment/comment/:id', () => {});
 
 // история оборудования
 router.get('/equipments/history/:equipmentId', () => {}); // @todo: пагинация
