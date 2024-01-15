807e9b397c3645554e8c8387a038d7dc86f1b1c8
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-01-15 17:53:42 +0600

    add: auth module api

---
+++ frontend/src/api/modules/auth.ts
@@ -0,0 +1,9 @@
+import type { AxiosInstance } from 'axios';
+import type { IAuthPayload } from '@/entities/types/backend/payload/authPayload';
+import type { ILoginResponse } from '@/entities/types/backend/response/auth';
+
+export const auth = (client: AxiosInstance) => ({
+  login: (payload: IAuthPayload): Promise<ILoginResponse> => {
+    return client.post(`/login`, payload);
+  },
+});
