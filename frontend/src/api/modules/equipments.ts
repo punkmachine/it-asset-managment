48da7200b7305c85f9a541b587f37b8658968361
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-01-15 17:54:03 +0600

    add: equipments module api

---
+++ frontend/src/api/modules/equipments.ts
@@ -0,0 +1,35 @@
+import type { AxiosInstance } from 'axios';
+import type { IQueryData } from '@/entities/types/backend/query';
+import type { IEquipmentCreatePayload, IEquipmentUpdatePayload, ICommentPayload, IHistoryPayload } from '@/entities/types/backend/payload/equipmentPayload';
+import type { IEquipment, IHistoryItem } from '@/entities/types/backend/response/equipment';
+import { getQueryParams } from '@/utils/helpers/queryParam';
+
+export const equipments = (client: AxiosInstance) => ({
+  fetchEquipments: (): Promise<IEquipment[]> => {
+    return client.get('/equipments');
+  },
+  getEquipmentById: (id: number | string): Promise<IEquipment> => {
+    return client.get(`/equipment/${id}`);
+  },
+  searchEquipment: (data: IQueryData): Promise<IEquipment[]> => {
+    return client.get(`equipments/search?${getQueryParams(data)}`);
+  },
+  createEquipment: (payload: IEquipmentCreatePayload): Promise<IEquipment> => {
+    return client.post('/equipment', payload);
+  },
+  updateEquipment: (id: number | string, payload: IEquipmentUpdatePayload): Promise<IEquipment> => {
+    return client.put(`/equipment/${id}`, payload);
+  },
+  deleteEquipment: (id: number | string): Promise<IEquipment> => {
+    return client.delete(`/equipment/${id}`);
+  },
+  commentEquipment: (equipmentId: number | string, payload: ICommentPayload): Promise<IEquipment> => {
+    return client.post(`/equipment/comment/${equipmentId}`, payload);
+  },
+  getEquipmentsHistory: (equipmentId: number | string): Promise<IHistoryItem> => {
+    return client.get(`/equipments/history/${equipmentId}`);
+  },
+  postEquipmentsHistory: (equipmentId: number | string, payload: IHistoryPayload): Promise<IHistoryItem> => {
+    return client.post(`/equipments/history/${equipmentId}`, payload);
+  }
+});
