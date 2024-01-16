a6ac0307b43acffc1170712cb84e8d7d6e3547c1
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-01-16 10:26:23 +0600

    upd: middleware.

--- frontend/src/router/middleware/index.ts
+++ frontend/src/router/middleware/index.ts
@@ -1,6 +1,7 @@
 import type { RouteLocationNormalized, NavigationGuardNext } from 'vue-router';
 import Cookies from 'js-cookie';
 import router from '@/router';
+import { useUsersStore } from '@/store';
 
 export default function (to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) {
   if (!to.meta.secure) {
@@ -10,11 +11,16 @@ export default function (to: RouteLocationNormalized, from: RouteLocationNormali
   }
 
   const token = Cookies.get('token');
+  const userStore = useUsersStore();
 
-  if (!token) {
+  if (!token || !userStore.currentUserId) {
     router.push('/login');
 
     return;
+  }
+
+  if (!userStore.currentUser) {
+    userStore.fetchCurrentUser();
   }
 
   next();
