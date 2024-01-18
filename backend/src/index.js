a9b9a257699bc56233f0a592c420e80e5b13a4c1
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-01-18 17:41:29 +0600

    upd: добавил политику cors на бэк

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,13 @@
+/*
+  catch middleware
+  валидации
+*/
+
 import express from 'express';
 import mongoose from 'mongoose';
 import fileUpload from 'express-fileupload';
+import cors from 'cors';
+
 import router from './router.js';
 
 const PORT = 5004;
@@ -8,8 +15,11 @@ const DB_URL = 'mongodb://127.0.0.1:27017/it-asset-managment';
 const app = express();
 
 app.use(fileUpload({}));
-app.use(express.static('static'));
+app.use(express.static('uploads'));
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173',
+}));
 app.use('/api', router);
 
 async function startApp() {
