29300f80104e5801d45d01159e96bd1ef7047cb6
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-01-24 08:25:13 +0600

    add: колонки для таблицы юзеров

--- frontend/src/views/users/settings.ts
+++ frontend/src/views/users/settings.ts
@@ -1,8 +1,10 @@
 import type { IColumn } from '@/entities/types/UI/table'
 
 export const columnsSettings: IColumn[] = [
-  { key: 'id', title: 'id', width: '120px' },
+  { key: 'number', title: 'Номер', width: '120px' },
   { key: 'FIO', title: 'ФИО' },
+  { key: 'role', title: 'Роль' },
+  { key: 'state', title: 'Состояние' },
 ];
 
 export const initialUser = {
