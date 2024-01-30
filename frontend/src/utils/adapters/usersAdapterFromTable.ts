c84799d3261d0024a07440ffc49e047813949234
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-01-31 04:36:47 +0600

    add: адаптер для данных о юзерах

--- frontend/src/utils/adapters/usersAdapterFromTable.ts
+++ frontend/src/utils/adapters/usersAdapterFromTable.ts
@@ -1,11 +1,35 @@
 import type { IUserInTable, ICell } from '@/entities/types/UI/table';
 import type { IUser } from '@/entities/types/backend/response/user';
+import { UserState, UserRoles } from '@/entities/types/backend/response/user';
+
+function getReadableRole(key: UserRoles | 'default') {
+  const dict = {
+    SUPERADMIN: 'Царь',
+    ADMIN: 'Администратор',
+    default: '...'
+  };
+
+  return dict[key] ? dict[key] : dict.default;
+}
+
+function getReadableState(key: UserState | 'default') {
+  const dict = {
+    ACTIVE: 'Активен',
+    DELETED: 'Заблокирован',
+    default: '...'
+  };
+
+  return dict[key] ? dict[key] : dict.default;
+}
 
 function usersAdapterFromTable(users: IUser[]): IUserInTable[] {
-  return users.map(user => {
+  return users.map((user, index) => {
     return {
-      id: user.id.toString(),
+      _id: user._id,
+      number: `${index + 1}`,
       fio: `${user.firstName} ${user.lastName}`,
+      role: getReadableRole(user.role),
+      state: getReadableState(user.state),
     }
   });
 }
@@ -14,9 +38,9 @@ export function getTableRows(users: IUser[]) {
   const usersTable = usersAdapterFromTable(users);
 
   return usersTable.map(user => {
-    let result: ICell[] = [];
+    const result: ICell[] = [];
 
-    for (let key in user) {
+    for (const key in user) {
       result.push({
         key,
         title: user[key as keyof IUserInTable],
