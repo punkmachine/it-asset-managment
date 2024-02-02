e9b65919d1ca17bbb67019e253d6037b2564c9f9
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-02-02 21:01:44 +0600

    add: eslint settings;

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -3,7 +3,12 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
   'extends': [
+    // @todo: рекомендуемые настройки для ts
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
@@ -11,5 +16,27 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  // plugins: {},
+  rules: {
+    // душные правила по коду
+    'max-depth': ['error', 4], // максимально количество вложенных блоков видимости
+    'max-lines': ['error', {
+      max: 300,
+      skipComments: true,
+    }], // максимальное количество строк в файле
+    'max-lines-per-function': ['error', {
+      max: 12,
+      skipBlankLines: true,
+      skipComments: true,
+      IIFEs: false,
+    }], // максимальное количество строк в функции
+    'max-nested-callbacks': ['error', 4], // максимальная глубина вложенных вызовов функций
+    'id-length': ['error', {
+      min: 1,
+      max: 16,
+      properties: 'never',
+    }], // минимальные и максимальные длины переменных и функций
+  },
+  // globals: {},
 }
