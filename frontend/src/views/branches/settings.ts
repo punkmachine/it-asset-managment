fe8fb5e9b4ce06b2f021234fcd1076ca92f220ad
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-02-06 20:57:37 +0600

    upd: settings branches

--- frontend/src/views/branches/settings.ts
+++ frontend/src/views/branches/settings.ts
@@ -1,8 +1,9 @@
 import type { IColumn } from '@/entities/types/UI/table';
 
 export const columnsSettings: IColumn[] = [
-  { key: 'id', title: 'id', width: '120px' },
-  { key: 'title', title: 'Название' },
+  { key: 'id', title: 'Номер', width: '120px' },
+  { key: 'title', title: 'Название', width: '240px' },
+  { key: 'state', title: 'Состояние', width: '120px' },
   { key: 'description', title: 'Описание' },
 ];
 
