2b4835ed9f5d3a4cbf4f9af2d535df4c27583b70
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-02-06 20:57:52 +0600

    upd: адаптер для оборудования

--- frontend/src/utils/adapters/equipmentsAdapterFromTable.ts
+++ frontend/src/utils/adapters/equipmentsAdapterFromTable.ts
@@ -1,17 +1,34 @@
 import type { IEquipmentInTable, ICell } from '@/entities/types/UI/table';
 import type { IEquipment } from '@/entities/types/backend/response/equipment';
+import { EquipmentState } from '@/entities/types/backend/response/equipment';
+import { getFormatDate, TimeFormatDict } from '@/utils/helpers/date';
+
+function getReadableState(key: EquipmentState | 'default') {
+  const dict = {
+    ACTIVE: 'Активен',
+    DELETED: 'Заблокирован',
+    default: '...'
+  };
+
+  return dict[key] ? dict[key] : dict.default;
+}
 
 function equipmentsAdapterFromTable(equipments: IEquipment[]): IEquipmentInTable[] {
-  return equipments.map(equipment => {
+  return equipments.map((equipment, index) => {
+    const { financiallyResponsiblePerson, branch } = equipment;
+
     return {
-      id: equipment.id.toString(),
+      _id: equipment._id,
+      number: `${index + 1}`,
       inventoryNumber: equipment.inventoryNumber,
       serialNumber: equipment.serialNumber,
       assetNumber: equipment.assetNumber,
       name: equipment.name,
-      createdDate: equipment.createdDate,
-      updatedDate: equipment.updatedDate,
-      financiallyResponsiblePerson: equipment.financiallyResponsiblePerson,
+      createdDate: getFormatDate(equipment.createdDate, TimeFormatDict.FullDateLongMonth),
+      updatedDate: getFormatDate(equipment.updatedDate, TimeFormatDict.FullDateLongMonth),
+      financiallyResponsiblePerson: `${financiallyResponsiblePerson.firstName} ${financiallyResponsiblePerson.lastName}`,
+      state: getReadableState(equipment.state),
+      branch: branch.title,
     }
   });
 }
@@ -20,9 +37,9 @@ export function getTableRows(equipments: IEquipment[]) {
   const equipmentsTable = equipmentsAdapterFromTable(equipments);
 
   return equipmentsTable.map(equipment => {
-    let result: ICell[] = [];
+    const result: ICell[] = [];
 
-    for (let key in equipment) {
+    for (const key in equipment) {
       result.push({
         key,
         title: equipment[key as keyof IEquipmentInTable],
