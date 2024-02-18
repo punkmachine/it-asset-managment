48719a0510320f094b91d4aa5f99267519aa4dc8
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-02-18 11:18:47 +0600

    upd: cors host in env

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(fileUpload({}));
 app.use(express.static('uploads'));
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.CORS_HOST,
 }));
 app.use('/api', router);
 
